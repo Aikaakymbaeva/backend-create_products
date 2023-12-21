89b7206735ffc435bf435cbda1ad1cc7c0d68306
Aihumushakymbaeva <aikaakymbaeva@gmail.com>
2023-12-21 11:54:40 +0100

    used useSwr and response

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -8,4 +8,11 @@ export default async function handler(request, response) {
     const products = await Product.find();
     return response.status(200).json(products);
   }
+  if (request.method === "POST") {
+    // send joke with POST fetch/request.
+    const productData = request.body;
+    // use jokeData to create joke in Database
+    await Product.create(productData);
+    return response.status(201).json({ status: "Yup! Product created!" });
+  }
 }
